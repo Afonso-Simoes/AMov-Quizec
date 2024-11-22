4f45f493a743d7c3715668eb87c27a3ca58737ca
Afonso Simões <afonsosimoes45@gmail.com>
2024-11-22 21:40:54 +0000

    Created the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "pt.isec.amov.quizectpamov"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "pt.isec.amov.quizectpamov"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
